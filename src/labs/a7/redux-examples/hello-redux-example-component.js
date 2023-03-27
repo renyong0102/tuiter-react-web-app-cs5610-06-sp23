a20ac856aeb2053c7027e96f85b9dcafdc140ad4
renyongzheng <zheng.ren@northeastern.edu>
2023-03-26 17:49:09 -0700

    a7 lab update

--- src/labs/a7/redux-examples/hello-redux-example-component.js
+++ src/labs/a7/redux-examples/hello-redux-example-component.js
@@ -1,12 +1,13 @@
 import React from "react";
 // uses redux's useSelector hook to extract the message from the reducer (react-redux)
 import {useSelector} from "react-redux";
+import hello from "./reducers/hello";
 
 // create a function called Hello...Component -> by useSelector, retrieve reducer's info
 // when component loads, reducers pass the data in function
 const HelloReduxExampleComponent = () => {
     // extract 'Hello World' from reducers->hello.js
-    const message = useSelector((state) => state.message);
+    const message = useSelector((state) => state.hello.message);
     return(
         <h3>{message}</h3>
     );
